ca1c57cd73d68b34fa0f26543e9c1d1ece753760
AnaBia25 <115107561+BeaSMonteiro@users.noreply.github.com>
2022-10-06 15:52:15 +0000

    Initial commit

---
+++ conversor.py
@@ -0,0 +1,5 @@
+def Polegadas():
+  cm = float(input("Digite o valor em centímetros: "))
+  pole = cm * 0.39370
+  file = open('Valores.txt','w+')
+  file.write(f'O valor {cm} em centímetros corresponde a {pole} valor em polegadas.')
